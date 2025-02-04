d1bf20f95fdeac7da7e8228a77cd6bcd8ab7c9f3
Devansh-001 <devanshsehgal44@gmail.com>
2025-02-05 00:38:09 +0530

    changed next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:["coin-images.coingecko.com"]
+        domains:["coin-images.coingecko.com"],
+        loader: 'default',
     }
     
 };
